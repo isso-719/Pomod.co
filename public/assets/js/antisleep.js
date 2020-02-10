47d73f00a0e9701782b7565ae715ee2b13fb9a15
Kazuki Isogai <kazu0719_i@ybb.ne.jp>
2020-02-10 15:35:42 +0900

    Remove assets

--- public/assets/js/antisleep.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("click", function() {
-  document.getElementById("sleep-prevent-video-ios").play();
-  document.getElementById("sleep-prevent-video-android").play();
-});
-
-setInterval(function() {
-  document.getElementById("sleep-prevent-video-ios").play();
-  document.getElementById("sleep-prevent-video-android").play();
-}, 20000);
